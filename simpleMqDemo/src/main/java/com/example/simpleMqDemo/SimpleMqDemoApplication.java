fbb571593fc1bf31c0547d425e586709e8a0ba17
Irian Carolina Joubert Castellanos <ijoubert@technisys.com>
2021-05-06 18:12:35 -0300

    Spring application with MQ sender/receiver simple example

---
+++ simpleMqDemo/src/main/java/com/example/simpleMqDemo/SimpleMqDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleMqDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMqDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMqDemoApplication.class, args);
+	}
+
+}
